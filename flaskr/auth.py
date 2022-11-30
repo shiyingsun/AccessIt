03c5b604477fdb45f41b93f4cfb71b665957b330
shiyingsun <08ssun@brightoncollege.net>
2022-11-30 10:27:33 +0000

    Added comments

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -10,32 +10,35 @@ from flaskr.db import get_db
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/register', methods=('GET', 'POST'))
+
 def register():
-    containsNumber = False
-    containsCapital = False
+    # this function allows the user to register a new account
+    containsNumber = False  # this bool variable checks if the password contains a number
+    containsCapital = False  # this bool variable checks if the password contains a capital letter
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        #email = request.form['email']
         db = get_db()
         error = None
 
+        # this if statement makes sure that both the username and password fields are filled
         if not username:
             error = 'Username is required.'
-        #elif not email:
-         #   error = 'Email is required'
         elif not password:
             error = 'Password is required.'
 
+        # this if statement makes sure the password is at least 8 characters
         if len(password) < 8:
             error = 'Password needs to contain at least 8 characters'
 
+        # makes sure the password contains a number and capital
         for i in password:
             if i.isdigit():
                 containsNumber = True
             if i.isupper():
                 containsCapital = True
 
+        # this throws errors if the password doesn't meet requirements
         if not containsNumber:
             error = 'Password needs to contain a number'
 
@@ -44,14 +47,17 @@ def register():
 
         if error is None:
             try:
+                # this adds the user to the database if there isn't an error
                 db.execute(
                     "INSERT INTO user (username, password) VALUES (?, ?)",
                     (username, generate_password_hash(password)),
                 )
                 db.commit()
             except db.IntegrityError:
+                # throw an error and doesn't allow user to proceed if the username is taken
                 error = f"User {username} is already registered."
             else:
+                # redirects user to login if registration is successful
                 return redirect(url_for("auth.login"))
 
         flash(error)
@@ -60,20 +66,24 @@ def register():
 
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
+    # this function
     if request.method == 'POST':
-        username = request.form['username']
+        username = request.form['username']  # this links to the HTML of the inputs
         password = request.form['password']
         db = get_db()
         error = None
+        # this looks for the username entered from the database
         user = db.execute(
             'SELECT * FROM user WHERE username = ?', (username,)
         ).fetchone()
 
+        # This if statement checks the entered username and password against the database
         if user is None:
             error = 'Incorrect username.'
         elif not check_password_hash(user['password'], password):
             error = 'Incorrect password.'
 
+        # if there are no errors, it allows the user to login and redirects to the 'posts' page
         if error is None:
             session.clear()
             session['user_id'] = user['id']
